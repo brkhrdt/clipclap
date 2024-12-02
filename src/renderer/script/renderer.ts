deb963a8823272978973072a733321ac9c8176c8
Burkhardt, Shawna <a@b.c>
2024-12-01 20:11:57 -0800

    didnt need this part when using monaco plugin

--- src/renderer/script/renderer.ts
+++ src/renderer/script/renderer.ts
@@ -73,25 +73,6 @@ window.electron.onClipboardUpdated((event: Event, history: Clip[]) => {
     updateHistory(history);
 });
 
-// @ts-ignore
-self.MonacoEnvironment = {
-	getWorkerUrl: function (moduleId, label) {
-		if (label === 'json') {
-			return './json.worker.js';
-		}
-		if (label === 'css' || label === 'scss' || label === 'less') {
-			return './css.worker.js';
-		}
-		if (label === 'html' || label === 'handlebars' || label === 'razor') {
-			return './html.worker.js';
-		}
-		if (label === 'typescript' || label === 'javascript') {
-			return './ts.worker.js';
-		}
-		return './editor.worker.js';
-	}
-};
-
 const editor = monaco.editor.create(document.getElementById('editor'), {
 	value: ''
 	// language: 'javascript'
