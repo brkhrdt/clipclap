2363f763ab79c00dc0775cb33d21a8b015421c2a
Burkhardt, Shawna <a@b.c>
2024-12-29 17:33:11 -0800

    make zip for all os

--- forge.config.ts
+++ forge.config.ts
@@ -18,7 +18,7 @@ const config: ForgeConfig = {
     rebuildConfig: {},
     makers: [
         new MakerSquirrel({}),
-        new MakerZIP({}, ['darwin']),
+        new MakerZIP({}),
         new MakerRpm({}),
         new MakerDeb({}),
     ],
